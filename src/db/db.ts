2c12540802544bdbda6ecdd333586b6d3448e3a4
Sanzadinternational <sanzadinternational5@gmail.com>
2025-04-15 14:56:42 +0530

    Update db.ts

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,5 +1,6 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
-import { Pool } from 'pg';
+import pkg from 'pg';
+const { Pool } = pkg;
 import { DB_URI } from '../config';
 
 const pool = new Pool({
