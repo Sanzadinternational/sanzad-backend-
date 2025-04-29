23174b67be39ea9f62363317dc831024720e37cd
Sanzadinternational <sanzadinternational5@gmail.com>
2025-04-29 17:07:41 +0530

    Update PaymentController.ts

--- src/controllers/PaymentController.ts
+++ src/controllers/PaymentController.ts
@@ -13,8 +13,8 @@ export const PaymentIniciate = async (req: Request, res: Response, next: NextFun
         const merchantId = process.env.CCAVENUE_MERCHANT_ID!;
         const accessCode = process.env.CCAVENUE_ACCESS_CODE!;
         const workingKey = process.env.CCAVENUE_WORKING_KEY!;
-        const redirectUrl = "https://sanzad.vercel.app/api/V1/payment/payment-status-update";
-        const cancelUrl = "https://sanzad.vercel.app/cancle";
+        const redirectUrl = "https://api.sanzadinternational.in/api/V1/payment/payment-status-update";
+        const cancelUrl = "https://sanzadinternational.in/cancle";
         const customerEmail = "abhinavgu34@gmail.com";
         const customerPhone = "8433169822";
 
