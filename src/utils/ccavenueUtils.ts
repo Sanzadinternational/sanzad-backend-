fc103166232201a241d21a8b2d6c70d043b9ee09
Sanzadinternational <sanzadinternational5@gmail.com>
2025-05-03 16:25:13 +0530

    Update ccavenueUtils.ts

--- src/utils/ccavenueUtils.ts
+++ src/utils/ccavenueUtils.ts
@@ -1,19 +1,19 @@
 import crypto from 'crypto';
 
 const encrypt = (plainText: string, workingKey: string): string => {
-  const key = crypto.createHash('md5').update(workingKey).digest(); // 16 bytes
-  const iv = Buffer.alloc(16, 0); // 16 zero bytes
-  const cipher = crypto.createCipheriv('aes-128-cbc', key, iv);
-  let encrypted = cipher.update(plainText, 'utf8', 'base64');
-  encrypted += cipher.final('base64');
+  const key = crypto.createHash('md5').update(workingKey).digest();
+  const iv = Buffer.alloc(16, 0);
+  const cipher = crypto.createCipheriv('aes-256-cbc', Buffer.concat([key, key.slice(0, 16)]), iv);
+  let encrypted = cipher.update(plainText, 'utf8', 'hex');
+  encrypted += cipher.final('hex');
   return encrypted;
 };
 
 const decrypt = (encryptedText: string, workingKey: string): string => {
   const key = crypto.createHash('md5').update(workingKey).digest();
   const iv = Buffer.alloc(16, 0);
-  const decipher = crypto.createDecipheriv('aes-128-cbc', key, iv);
-  let decrypted = decipher.update(encryptedText, 'base64', 'utf8');
+  const decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.concat([key, key.slice(0, 16)]), iv);
+  let decrypted = decipher.update(encryptedText, 'hex', 'utf8');
   decrypted += decipher.final('utf8');
   return decrypted;
 };
