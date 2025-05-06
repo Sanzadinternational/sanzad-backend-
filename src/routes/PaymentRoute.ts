743abeb3270f4cc5e043f384d54fb26788e1bb3f
Sanzadinternational <sanzadinternational5@gmail.com>
2025-05-06 13:38:21 +0530

    Update PaymentRoute.ts

--- src/routes/PaymentRoute.ts
+++ src/routes/PaymentRoute.ts
@@ -1,11 +1,11 @@
 import express, {Request, Response, NextFunction, Router} from 'express'; 
-import { PaymentIniciate, PaymentWithReferenceNo } from '../controllers/PaymentController';
+import { PaymentIniciate, PaymentWithReferenceNo,ChangePaymentStatusByBookingId } from '../controllers/PaymentController';
 import { PaymentStatusUpdate } from '../controllers/PaymentController';
 const router = express.Router(); 
 
 router.post('/payment-iniciate', PaymentIniciate); 
 router.post('/payment-status-update', PaymentStatusUpdate);
 router.post('/referencePayment', PaymentWithReferenceNo);
-
+router.put('/ChangePaymentStatusByBookingId/:id', ChangePaymentStatusByBookingId);
 
 export {router as PaymentRoute}; 
