959c91a02a2fd2b07b71a945c5d9a02ed58eaf13
Sanzadinternational <sanzadinternational5@gmail.com>
2025-05-16 11:57:02 +0530

    Update PaymentController.ts

--- src/controllers/PaymentController.ts
+++ src/controllers/PaymentController.ts
@@ -53,7 +53,8 @@ export const PaymentIniciate = async (req: Request, res: Response, next: NextFun
   const bookingId = booking.id;
 
   // Step 2: Generate Order ID for CCAvenue
-  const orderId = `BOOK${bookingId}${Date.now()}`;
+const orderId = `BOOK-${bookingId.slice(0, 8)}-${Date.now().toString().slice(-4)}`;
+
 
         // Payment data
         const data = `merchant_id=${merchantId}&order_id=${orderId}&currency=INR&amount=1&redirect_url=${redirectUrl}&cancel_url=${cancelUrl}&billing_email=${customerEmail}&billing_tel=${customerPhone}&merchant_param1=${bookingId}`;
