771a9afba1c6c9921bd2a0586c09ae7ba0afe764
Sanzadinternational <sanzadinternational5@gmail.com>
2025-06-02 17:23:59 +0530

    Update SupplierController.ts

--- src/controllers/SupplierController.ts
+++ src/controllers/SupplierController.ts
@@ -1689,6 +1689,7 @@ export const getTransferBySupplierId = async (req: Request, res: Response) => {
             parking:transfers_Vehicle.parking,
             driverTips: transfers_Vehicle.driverTips,
             driverCharge:transfers_Vehicle.driverCharge,
+            Distance:transfers_Vehicle. Distance,
             Zone_name:zones.name,
             VehicleType:Create_Vehicles.VehicleType,
             VehicleBrand:Create_Vehicles.VehicleBrand,
