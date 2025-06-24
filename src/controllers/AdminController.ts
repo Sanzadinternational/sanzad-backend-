a3a746da633b61f8ab487a2f63459c0fe89509cc
Sanzadinternational <sanzadinternational5@gmail.com>
2025-06-24 17:32:42 +0530

    Update AdminController.ts

--- src/controllers/AdminController.ts
+++ src/controllers/AdminController.ts
@@ -499,6 +499,15 @@ export const ChangeSupplierApprovalStatus = async(req:Request,res:Response,next:
         res.status(200).json({message:"Supplier Status is updated Successfully",result,results})
   }else if(parseInt(isApproved) === 2)
     {
+         const { Email, RejectionReason } = req.body;
+
+        const result = await db.update(registerTable)
+        .set({ RejectionReason })
+        .where(eq(registerTable.Email, Email)) // or use `.id` if you're using ID
+        .returning();
+
+        res.status(200).json({message:"Supplier is Rejected Successfully",result});
+        
     const info = await transporter.sendMail({
       from: '"Sanzad International" <sanzadinternational5@gmail.com>', // Sender address
       to: `${result[0].Email}`, // Recipient email
