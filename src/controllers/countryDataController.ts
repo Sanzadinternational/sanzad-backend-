2ee038362f58def591ce27f81dbd17859e64783e
Sanzadinternational <sanzadinternational5@gmail.com>
2025-06-25 11:05:05 +0530

    Create countryDataController.ts

---
+++ src/controllers/countryDataController.ts
@@ -0,0 +1,9 @@
+import { Request, Response, NextFunction } from "express";
+const raw = fs.readFileSync(new URL('./country.json', import.meta.url), 'utf-8');
+const data = JSON.parse(raw);
+
+
+
+export const getAllCountryData = async (req: Request, res: Response, next: NextFunction) => {
+res.json(data);
+}
