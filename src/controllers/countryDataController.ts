c905525c15e5ac0f322ab26ef96d93a42aaa339f
Sanzadinternational <sanzadinternational5@gmail.com>
2025-06-25 11:53:35 +0530

    Update countryDataController.ts

--- src/controllers/countryDataController.ts
+++ src/controllers/countryDataController.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import fs from 'fs';
+import path from "path";
 const raw = fs.readFileSync(new URL('./country.json', import.meta.url), 'utf-8');
 const data = JSON.parse(raw);
 
