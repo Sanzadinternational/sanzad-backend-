c13448c6cda276a9cf1f8d96af5c1ecd684d0334
Sanzadinternational <sanzadinternational5@gmail.com>
2025-06-25 11:16:43 +0530

    Update countryRoute.ts

--- src/routes/countryRoute.ts
+++ src/routes/countryRoute.ts
@@ -1,5 +1,5 @@
 import express, {Request, Response, NextFunction, Router} from 'express'; 
-import getAllCountryData from "../controllers/countryDataController";
+import {getAllCountryData} from "../controllers/countryDataController";
 const router = express.Router(); 
 
 router.get('/GetCountryData',getAllCountryData);
