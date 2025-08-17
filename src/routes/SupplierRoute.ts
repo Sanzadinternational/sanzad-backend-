78f09e9584628c6bfbc99ce5ffcc64990cf6a9a5
Sanzadinternational <sanzadinternational5@gmail.com>
2025-08-17 08:22:32 +0530

    Update SupplierRoute.ts

--- src/routes/SupplierRoute.ts
+++ src/routes/SupplierRoute.ts
@@ -7,9 +7,6 @@ import multer from 'multer';
 import fs from 'fs';
 import path from 'path';
 const uploadDir = "/uploads";
-if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir, { recursive: true });
-}
 
 // Configure Multer
 const storage = multer.diskStorage({
